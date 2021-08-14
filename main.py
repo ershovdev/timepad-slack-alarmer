a77c4cd5a846e193eacb7eff001ac091f57d62a1
Mikhail <mihaershv@gmail.com>
2021-08-14 19:40:19 +0300

    order webhook stub

--- main.py
+++ main.py
@@ -22,7 +22,8 @@ app.logger.setLevel(logging.INFO)
 
 slack_bot_token = os.getenv("SLACK_BOT_TOKEN")
 slack_bot_channel_id = os.getenv("SLACK_BOT_CHANNEL_ID")
-secret_url_path = os.getenv("SECRET_URL_PATH")
+secret_url_ticket_path = os.getenv("SECRET_URL_TICKET_PATH")
+secret_url_order_path = os.getenv("SECRET_URL_ORDER_PATH")
 secret_timepad_phrase = os.getenv("SECRET_TIMEPAD_PHRASE")
 
 client = WebClient(token=slack_bot_token)
@@ -34,8 +35,8 @@ def log_request_info():
     app.logger.debug('Body\n---\n%s', request.get_data())
 
 
-@app.route('/{}'.format(secret_url_path), methods=["POST"])
-def timepad_webhook():
+@app.route('/{}'.format(secret_url_ticket_path), methods=["POST"])
+def timepad_ticket_webhook():
     update = request.get_json()
 
     sig = request.headers.get('X-Hub-Signature') or ''
@@ -51,7 +52,7 @@ def timepad_webhook():
         return "OK", 200
 
     try:
-        message = get_slack_message(update)
+        message = get_ticket_slack_message(update)
 
         client.chat_postMessage(
             channel=slack_bot_channel_id,
@@ -63,7 +64,7 @@ def timepad_webhook():
     return "OK", 200
 
 
-def get_slack_message(timepad_json):
+def get_ticket_slack_message(timepad_json):
     name = timepad_json['name']
     surname = timepad_json['surname']
     email = timepad_json['email']
@@ -76,6 +77,34 @@ def get_slack_message(timepad_json):
 :dollar: %s₽
 :student: %s %s (%s)
 Статус: %s""" % (event_name, price, name, surname, email, status)
+
+
+@app.route('/{}'.format(secret_url_order_path), methods=["POST"])
+def timepad_order_webhook():
+    update = request.get_json()
+
+    sig = request.headers.get('X-Hub-Signature') or ''
+    sig_gen = hmac.new(str.encode(secret_timepad_phrase), request.get_data(), hashlib.sha1).hexdigest()
+
+    if sig != ("sha1=" + sig_gen):
+        response = json.dumps({'message': 'Authentication Required'})
+        return response, 401
+
+    try:
+        message = get_order_slack_message(update)
+
+        client.chat_postMessage(
+            channel=slack_bot_channel_id,
+            text=message
+        )
+    except SlackApiError as e:
+        assert e.response["error"]
+
+    return "OK", 200
+
+
+def get_order_slack_message(timepad_json):
+    return json.dumps(timepad_json, ensure_ascii=True, indent=4)
 
 
 if __name__ == '__main__':
